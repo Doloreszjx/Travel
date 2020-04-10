c071e2c8805c925cc98b3db33977c3098e742e38
Doloreszjx <1173827505@qq.com>
2020-04-10 16:59:36 +0800

    project detail-list-logic

--- Travel/src/pages/detail/Detail.vue
+++ Travel/src/pages/detail/Detail.vue
@@ -2,18 +2,39 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/banner'
 import DetailHeader from './components/header'
+import DetailList from './components/list'
+
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人特惠一区'
+        }, {
+          title: '成人特惠二区'
+        }]
+      }, {
+        title: '儿童票'
+      }, {
+        title: '老人票'
+      }]
+    }
   }
 }
 </script>
---
+++ Travel/src/pages/detail/components/list.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <div
+        class="item"
+        v-for="(item, index) in list"
+        :key="index">
+        <div class="item-title border-bottom">
+          <span class="item-title-icon"></span>
+          {{item.title}}
+        </div>
+        <div v-if="item.children" class="item-chilren">
+          <detail-list :list="item.children"></detail-list>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.item
+  background-color: pink
+  .item-title
+    line-height: .8rem
+    font-size: .32rem
+    padding: 0 .2rem
+    .item-title-icon
+      position: relative
+      display: inline-block
+      left: .6rem
+      top: .6rem
+      width: .32rem
+      height: .32rem
+      margin-right: .1rem
+      background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+      background-size: .3rem .4rem
+  .item-chilren
+    padding: 0 .2rem
+</style>
